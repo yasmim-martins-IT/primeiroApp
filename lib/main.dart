1fac224941cd8a1856827f8375c1ba157fe868c2
yasmim-martins-IT <yasmim.martins.profissional@gmail.com>
2025-09-24 13:48:19 -0300

    colocando o body

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ void main() {
   runApp(const MyApp());
 }
 
+//COMANDO PARA RODAR O APLICATIVO : flutter run
+
+//--------------------------------------------------------------------------------------------------------------------------
+
 // runApp --->  a função run app serve para rodar a aplicação
 
 // St--> cria uma tela , toda a tela é a uma classe que possui o metodo construtor
@@ -20,6 +24,30 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // O MaterialApp é uma classe que prove os componentes para a tela
+
+      // scaffold separa a tela em partes para criar uuma estrutura , ele consegue fazer em até 3 partes
+
+      home: Scaffold(
+
+        //appBar é a barrinha que fica em cima da tela
+        appBar: AppBar(title:Text("Hello world") , backgroundColor: Colors.blueAccent, ) ,
+
+        //body é o conteudo da página
+
+        body: Container(
+
+          // Container é a caixinha que possui as seguintes propriedades : width e height
+          width: 50,
+          height: 50,
+
+          //trocando a cor : para trocar a cor existe duas maneiras
+          // Colors.nomeDaCor
+          //Color(0XFHexadecimalDaCor)
+
+          color: Colors.white,
+        ),
+
+      )
     ) ;
   }
 }
